140a6d2f809831cc7561021041e94b4a3689a3aa
northlandcaving <cavesareundergr0und@protonmail.com>
2025-02-20 21:59:34 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+
+
+var x = 0
+
+function p1ChgFn() {
+
+
+    if (x == 0) {let par1 = document.getElementById("para1");
+    par1.textContent = "new text"; x = 1;
+    } else {let par1 = document.getElementById("para1");
+        par1.textContent = "Welcome to the Northland Underground Republic (NUR), a club for people unaffected by claustrophobia."; x = 0;}
+    
+    
+
+    
+}
