5ec4f74ac74937226bae20540e7e885b5cab9cc5
northlandcaving <cavesareundergr0und@protonmail.com>
2025-02-21 23:09:31 -0300

    v0.94
    script containing 2 pages

--- scripts.js
+++ scripts.js
@@ -1,17 +1,248 @@
 
-
-
 var x = 0
+
+
+
 
 function p1ChgFn() {
 
 
-    if (x == 0) {let par1 = document.getElementById("para1");
-    par1.textContent = "new text"; x = 1;
-    } else {let par1 = document.getElementById("para1");
-        par1.textContent = "Welcome to the Northland Underground Republic (NUR), a club for people unaffected by claustrophobia."; x = 0;}
-    
-    
+    if (x == 0) {document.getElementById('myBody').innerHTML = '<link rel="stylesheet" href="https://northlandcaving.github.io/caving/photostyles.css">  \n \
+        \n \
+		<div class="dropdown"> \n \
+            <button class="dropbtn">☰ Caving Photos</button> \n \
+            <div> \n \
+         \n \
+        		<button class = "dropdown-content" type = "button" onclick="p1ChgFn()"> \n \
+                	> Main \n \
+            	</button> \n \
+            	<script src="https://northlandcaving.github.io/scripts.js"></script>      \n \
+         \n \
+    		</div> \n \
+  		</div> \n \
+		 \n \
+        <div class = "container">   \n \
+            \n \
+            <div class = "picheading"><h1>Caving Trip Pictures:</h1></div>   \n \
+\n \
+    \n \
+            <div class = "pic0"><img  src = "https://northlandcaving.github.io/caving/pic0.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"></div>  \n \
+    \n \
+            <div class = "pic1"><img  src = "https://northlandcaving.github.io/caving/pic1.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"> </div> \n \
+            \n \
+            <div class = "pic2"> <img src = "https://northlandcaving.github.io/caving/pic2.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"></div> \n \
+            \n \
+            <div class = "pic3"> <img src = "https://northlandcaving.github.io/caving/pic3.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"> </div> \n \
+ \n \
+            <div class = "pic4">                <img src = "https://northlandcaving.github.io/caving/pic4.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic5">                <img src = "https://northlandcaving.github.io/caving/pic5.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic6">                <img src = "https://northlandcaving.github.io/caving/pic6.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic7">                <img src = "https://northlandcaving.github.io/caving/pic7.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic8">                <img src = "https://northlandcaving.github.io/caving/pic8.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic9">                <img src = "https://northlandcaving.github.io/caving/pic9.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">           </div> \n \
+ \n \
+ \n \
+            <div class = "pic10">                <img src = "https://northlandcaving.github.io/caving/pic10.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic11">                <img  src = "https://northlandcaving.github.io/caving/pic11.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic12">                <img src = "https://northlandcaving.github.io/caving/pic12.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic13">               <img src = "https://northlandcaving.github.io/caving/pic13.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic14">                <img src = "https://northlandcaving.github.io/caving/pic14.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic15">                <img src = "https://northlandcaving.github.io/caving/pic15.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic16">                <img src = "https://northlandcaving.github.io/caving/pic16.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic17">                <img src = "https://northlandcaving.github.io/caving/pic17.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic18">                <img src = "https://northlandcaving.github.io/caving/pic18.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic19">                <img src = "https://northlandcaving.github.io/caving/pic19.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">           </div> \n \
+ \n \
+ \n \
+            <div class = "pic20">                <img src = "https://northlandcaving.github.io/caving/pic20.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">           </div> \n \
+ \n \
+ \n \
+            <div class = "pic21">                <img  src = "https://northlandcaving.github.io/caving/pic21.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic22">                <img src = "https://northlandcaving.github.io/caving/pic22.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic23">                <img src = "https://northlandcaving.github.io/caving/pic23.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">           </div> \n \
+ \n \
+            <div class = "pic24">                <img src = "https://northlandcaving.github.io/caving/pic24.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic25">               <img src = "https://northlandcaving.github.io/caving/pic25.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic26">                <img src = "https://northlandcaving.github.io/caving/pic26.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic27">                <img src = "https://northlandcaving.github.io/caving/pic27.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic28">                <img src = "https://northlandcaving.github.io/caving/pic28.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic29">                <img src = "https://northlandcaving.github.io/caving/pic29.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">           </div> \n \
+ \n \
+             \n \
+            <div class = "pic30">                <img src = "https://northlandcaving.github.io/caving/pic30.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic31">                <img src = "https://northlandcaving.github.io/caving/pic31.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">           </div> \n \
+ \n \
+            <div class = "pic32">                <img src = "https://northlandcaving.github.io/caving/pic32.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">           </div> \n \
+ \n \
+            <div class = "pic33">                <img src = "https://northlandcaving.github.io/caving/pic33.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic34">                <img src = "https://northlandcaving.github.io/caving/pic34.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic35">                <img src = "https://northlandcaving.github.io/caving/pic35.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic36">                <img src = "https://northlandcaving.github.io/caving/pic36.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic37">                <img src = "https://northlandcaving.github.io/caving/pic37.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic38">                <img src = "https://northlandcaving.github.io/caving/pic38.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic39">                <img src = "https://northlandcaving.github.io/caving/pic39.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic40">                <img src = "https://northlandcaving.github.io/caving/pic40.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic41">                <img src = "https://northlandcaving.github.io/caving/pic41.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+             \n \
+            <div class = "pic42">                <img src = "https://northlandcaving.github.io/caving/pic42.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic43">                <img src = "https://northlandcaving.github.io/caving/pic43.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div> \n \
+ \n \
+            <div class = "pic44">                <img src = "https://northlandcaving.github.io/caving/pic44.jpg" alt = "Nothing to show" style = "width:250px;height:250px;">            </div>';
 
+            x = 1;
+    } else {document.getElementById('myBody').innerHTML = ' \n \
+ \n \
+        <div class="dropdown"> \n \
+            <button class="dropbtn">☰ Main</button> \n \
+            <div> \n \
+         \n \
+        		<button class = "dropdown-content" type = "button" onclick="p1ChgFn()"> \n \
+                	> Caving Photos\n \
+            	</button> \n \
+            	<script src="https://northlandcaving.github.io/scripts.js"></script>      \n \
+         \n \
+    		</div> \n \
+  		</div> \n \
+        \n \
+       <div id = "myDIV" class = "sectionstyle" > \n \
+            <!-- page section divided into its own part, has mainstyle class applied --> \n \
+ \n \
+            <h1>Northland Underground Republic</h1> \n \
+ \n \
+            <!-- first page heading --> \n \
+ \n \
+            <p1 id = "para1" >Welcome to the Northland Underground Republic (NUR), a club for people unaffected by claustrophobia.</p1> \n \
+ \n \
+            </div> \n \
+        \n \
+ \n \
+<!-- end of divided section --> \n \
+ \n \
+    <div class = "container"> \n \
+ \n \
+        <div class = "newsupdates"> \n \
+ \n \
+            <h1 class = "heading-align">Recent club news and activities:</h1> \n \
+ \n \
+<dl>  \n \
+    <dt>14-02-25</dt> \n \
+<dd>>Club formed! Site is still under development but is accessible on internet</dd> \n \
+    <dt>15-02-25</dt> \n \
+<dd>>Issues with CSS styling in HTML file, not currently fixed</dd> \n \
+    <dt>17-02-2025</dt> \n \
+<dd>>Some additions to web page</dd> \n \
+</dl> \n \
+ \n \
+        </div> \n \
+ \n \
+        <div class = "newsletter"> \n \
+ \n \
+            <h1>Club Newsletter</h1> \n \
+ \n \
+             \n \
+        </div> \n \
+ \n \
+        <div class = "pictures"> \n \
+ \n \
+            <div class = "picheading"><h1>Caving Trip Pictures:</h1></div> \n \
+     \n \
+            <div class = "pic1"> \n \
+                <img  src = "https://northlandcaving.github.io/caving/pic1.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"> \n \
+     \n \
+            </div> \n \
+     \n \
+            <div class = "pic2"> \n \
+                <img src = "https://northlandcaving.github.io/caving/pic2.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"> \n \
+     \n \
+            </div> \n \
+     \n \
+            <div class = "pic3"> \n \
+                <img src = "https://northlandcaving.github.io/caving/pic3.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"> \n \
+     \n \
+            </div> \n \
+     \n \
+            <div class = "pic4"> \n \
+                <img src = "https://northlandcaving.github.io/caving/pic4.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"> \n \
+     \n \
+            </div> \n \
+     \n \
+            <div class = "pic5"> \n \
+                <img src = "https://northlandcaving.github.io/caving/pic5.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"> \n \
+     \n \
+            </div> \n \
+     \n \
+            <div class = "pic6"> \n \
+                <img src = "https://northlandcaving.github.io/caving/pic6.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"> \n \
+                \n \
+     \n \
+            </div> \n \
+     \n \
+            <div class = "pic7"> \n \
+                <img src = "https://northlandcaving.github.io/caving/pic7.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"> \n \
+     \n \
+     \n \
+            </div> \n \
+     \n \
+            <div class = "pic8"> \n \
+                <img src = "https://northlandcaving.github.io/caving/pic8.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"> \n \
+     \n \
+     \n \
+            </div> \n \
+     \n \
+            <div class = "pic9"> \n \
+                <img src = "https://northlandcaving.github.io/caving/pic9.jpg" alt = "Nothing to show" style = "width:250px;height:250px;"> \n \
+     \n \
+     \n \
+            </div> \n \
+            \n \
+            <div class = "otherpagelink"> \n \
+            <button type = "button" onclick="p1ChgFn()"> \n \
+                All Photos... \n \
+            </button> \n \
+            <script src="https://northlandcaving.github.io/scripts.js"></script>      \n \
+            </div> \n \
+             \n \
+        </div> \n \
+ \n \
+  </div>'; x = 0
+
+    }
+    
+    
     
 }
+
+
+
